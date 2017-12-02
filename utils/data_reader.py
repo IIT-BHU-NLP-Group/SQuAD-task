1f4f89bb08d99263c4b792357a9a1e83456c981e
Divesh Pandey <divesh.pandey.cse15@iitbhu.ac.in>
2017-12-03 01:56:04 +0530

    Adds class definition for reading processed CSV file

---
+++ utils/data_reader.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from random import shuffle
+
+class DataReader:
+	def __init__(
+			self, 
+			file_path,
+			train_split_ratio=0.80,
+			dev_split_ratio=0.10,
+			test_split_ratio=0.10,
+			debug_mode=False, percent_debug_data=10):
+		self.file_path = file_path
+
+		self.read_data(
+			train_split_ratio, dev_split_ratio, test_split_ratio, 
+			debug_mode, percent_debug_data)
+
+	def read_data(
+			self, 
+			train_split_ratio, dev_split_ratio, test_split_ratio, 
+			debug_mode, percent_debug_data):
+
+		assert (train_split_ratio + dev_split_ratio + test_split_ratio) == 1
+
+		# Read CSV file
+		df = pd.read_csv(self.file_path, delimiter=",")
+		self.data = df.to_dict('records')
+		# Now, `self.data` is a list of dict
+		shuffle(self.data)
+
+		if debug_mode:
+			self.data = self.data[:percent_debug_data * 0.01 * len(self.data)]
+
+		data_size = len(self.data)
+		self.train = self.data[:int(train_split_ratio*data_size)]
+		self.dev = self.data[
+						int(train_split_ratio * data_size):
+						int((train_split_ratio + dev_split_ratio) * data_size)]
+		self.test = self.data[int(- (test_split_ratio) * data_size):]
+
+		self.data_dict = {
+			'train':	self.train,
+			'dev':		self.dev,
+			'test':		self.test}
+
+	def get_minibatch(self, batch_size):
+		# Useful function to return minibatches of training data
+		train_size = len(self.train)
+
+		assert batch_size < train_size
+
+		count = train_size // batch_size
+
+		for i in range(count):
+			yield self.train[i*batch_size : (i+1)*batch_size]
+
+	def get_complete_batch(self, choice):
+		assert (choice == 'train' or choice == 'dev' or choice == 'test')
+
+		return self.data_dict[choice]
+
+
+
+
+
+
+
+
+
